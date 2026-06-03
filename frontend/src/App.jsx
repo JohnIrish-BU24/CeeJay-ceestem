96a8628b4fc0523478b285e922a739f9f013ab6f
JohnIrish-BU24 <johnirishmemoracion.milallos24@bicol-u.edu.ph>
2026-06-03 10:45:03 +0800

    Di matapos na adjustments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -31,7 +31,7 @@ function App() {
       // REAL BACKEND AUTHENTICATION LOGIC
       // ==========================================
       try {
-        const response = await fetch('http://localhost:5000/api/employee/login', {
+        const response = await fetch('https://ceejay-ceestem.onrender.com/api/employee/login',  {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
